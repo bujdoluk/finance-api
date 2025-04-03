d7cc68573f200e6d6fc045ae60de85316f404c45
Lukáš Bujdoš <lukas.bujdos@gmail.com>
2025-04-03 20:00:51 +0200

    feat: added jsonlog, server and routes files

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"flag"
+	"os"
+
+	"github.com/bujdoluk/finance-api/internal/jsonlog"
+)
+
+type config struct {
+	port int
+	env  string
+}
+type application struct {
+	config config
+	logger *jsonlog.Logger
+}
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Enviroment (development|staging|production)")
+	flag.Parse()
+
+	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	err = app.serve()
+	if err != nil {
+		logger.PrintFatal(err, nil)
+	}
+}
---
+++ internal/jsonlog/jsonlog.go
@@ -0,0 +1,110 @@
+package jsonlog
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+	"runtime/debug"
+	"sync"
+	"time"
+)
+
+type Level int8
+
+const (
+	LevelDebug Level = iota
+	LevelInfo
+	LevelWarning
+	LevelError
+	LevelFatal
+	LevelOff
+)
+
+func (l Level) String() string {
+	switch l {
+	case LevelDebug:
+		return "DEBUG"
+	case LevelInfo:
+		return "INFO"
+	case LevelWarning:
+		return "WARNING"
+	case LevelError:
+		return "ERROR"
+	case LevelFatal:
+		return "FATAL"
+	default:
+		return ""
+	}
+}
+
+type Logger struct {
+	out      io.Writer
+	minLevel Level
+	mu       sync.Mutex
+}
+
+func New(out io.Writer, minLevel Level) *Logger {
+	return &Logger{
+		out:      out,
+		minLevel: minLevel,
+	}
+}
+
+func (l *Logger) PrintDebug(message string, properties map[string]string) {
+	l.print(LevelDebug, message, properties)
+}
+
+func (l *Logger) PrintInfo(message string, properties map[string]string) {
+	l.print(LevelInfo, message, properties)
+}
+
+func (l *Logger) PrintWarning(message string, properties map[string]string) {
+	l.print(LevelWarning, message, properties)
+}
+
+func (l *Logger) PrintError(err error, properties map[string]string) {
+	l.print(LevelError, err.Error(), properties)
+}
+
+func (l *Logger) PrintFatal(err error, properties map[string]string) {
+	l.print(LevelFatal, err.Error(), properties)
+	os.Exit(1)
+}
+func (l *Logger) print(level Level, message string, properties map[string]string) (int, error) {
+	if level < l.minLevel {
+		return 0, nil
+	}
+
+	aux := struct {
+		Level      string            `json:"level"`
+		Message    string            `json:"message"`
+		Properties map[string]string `json:"properties,omitempty"`
+		Trace      string            `json:"trace,omitempty"`
+		Time       string            `json:"time"`
+	}{
+		Level:      level.String(),
+		Message:    message,
+		Properties: properties,
+		Time:       time.Now().UTC().Format(time.RFC3339),
+	}
+
+	if level >= LevelError {
+		aux.Trace = string(debug.Stack())
+	}
+
+	var line []byte
+
+	line, err := json.Marshal(aux)
+	if err != nil {
+		line = []byte(LevelError.String() + ": unable to marshal log message:" + err.Error())
+	}
+
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	return l.out.Write(append(line, '\n'))
+}
+
+func (l *Logger) Write(message []byte) (n int, err error) {
+	return l.print(LevelError, string(message), nil)
+}
